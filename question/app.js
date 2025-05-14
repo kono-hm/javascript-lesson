97ffc1c25d577f8ab2c7367dbcddabe4b4f468de
omochi <omochi@example.com>
2025-05-14 15:06:50 +0900

    練習問題完了→次回応用練習

--- question/app.js
+++ question/app.js
@@ -1,3 +1,4 @@
+// ＊練習問題＊
 // Q1 変数
 let nickname = 'ごっしー';
 let age = 28;
@@ -72,3 +73,45 @@ let user = {
 user.sayHallo(); //Hello!
 
 // Q8 引数
+let calc = {};
+function add(x,y){
+  console.log(x + y);
+}
+add()
+
+let calc = {};
+function subtract(x,y){
+  console.log(x - y);
+}
+subtract(20,10)
+
+let calc = {};
+function multiply(x,y){
+    console.log(x * y);
+}
+multiply(7,7)
+
+
+let calc = {};
+function divide(x,y){
+    console.log(x / y);
+}
+divide(10,2)
+
+
+// Q9 返り値
+function remainder(x,y){
+  let resalt = (x % y);
+  return resalt;
+}
+console.log('5を3で割った余りは' + remainder(5,3) + 'です。');
+
+
+// Q10 スコープ
+// fooが関数として定義されているため、変数xのスコープは{}内となる。
+// よって、関数内に存在しないconsole.log(x);は、変数に値を代入しても参照外となる。
+
+
+
+
+// ＊応用編問題＊
